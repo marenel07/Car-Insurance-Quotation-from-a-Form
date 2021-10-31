456e833aa81043387bfc12b00e861d0471f584c0
marenel07 <villaralbonel29@gmail.com>
2021-10-31 12:34:46 -0700

    Displaying a Summary to app.js

--- START-insurance-main/js/app.js
+++ START-insurance-main/js/app.js
@@ -35,7 +35,7 @@ form.addEventListener('submit', function(e) {
         const price = insurance.calculateQuotation(insurance);
 
         // Print the result from HTMLUI();
-        html.showResults(price);
+        html.showResults(price, insurance);
     }
    
 
@@ -164,15 +164,37 @@ HTMLUI.prototype.displayError = function(message) {
 }
 
 // Print the result into the HTML
-HTMLUI.prototype.showResults = function(price) {
+HTMLUI.prototype.showResults = function(price, insurance) {
     //Print the result
     const result = document.getElementById('result');
 
     // Create a div with the result
     const div = document.createElement('div');
 
+
+    // Get make from the object and assign a readable name
+    let make = insurance.make;
+
+    switch(make) {
+        case '1':
+            make = 'American';
+            break;
+        case '2':
+            make = 'Asian';
+            break;
+        case '3':
+            make = 'European';
+            break;
+    }
+
+    console.log(make);
+
     // Insert the result
     div.innerHTML = `
+        <p class="header">Summary</p>
+        <p>Make: ${make}</p>
+        <p>Year: ${insurance.year}</p>
+        <p>Level: ${insurance.level}</p>
         <p class="total">Total: $ ${price}</p>
     `;
 
