976734dff0fdf23d91c65b773bec0f44849887e8
marenel07 <villaralbonel29@gmail.com>
2021-10-31 09:06:54 -0700

    Adding option in the Years to app.js

---
+++ START-insurance-main/js/app.js
@@ -0,0 +1,35 @@
+//Variable
+
+
+
+
+//Event Listeners
+document.addEventListener('DOMContentLoaded', function(){
+    //Create the options for the years
+    const html = new HTMLUI();
+    html.displayYears();
+});
+
+
+
+//Object
+
+function HTMLUI() {}
+
+//Displays the latest 20 years in the select
+HTMLUI.prototype.displayYears = function() {
+     // Max and Min years
+     const max = new Date().getFullYear(),
+           min = max - 20;
+     
+           //Generate the list with the latest 20 years
+           const selectYears = document.getElementById('year');
+
+           //Print the values
+           for(let i = max; i > min; i--) {
+               const option = document.createElement('option');
+               option.value = i;
+               option.textContent = i;
+               selectYears.appendChild(option);
+           }
+}
