9c818098b99d923a19af2f0b99285f4fe6afb4ef
marenel07 <villaralbonel29@gmail.com>
2021-10-31 09:51:20 -0700

    Reading Values from the Form to app.js

--- START-insurance-main/js/app.js
+++ START-insurance-main/js/app.js
@@ -1,14 +1,42 @@
 //Variable
-
+const form = document.getElementById('request-quote');
 
 
 
 //Event Listeners
+eventListeners();
+function eventListeners() {
+
+
 document.addEventListener('DOMContentLoaded', function(){
     //Create the options for the years
     const html = new HTMLUI();
     html.displayYears();
 });
+
+//When the form is submitted
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Read the values from the FORM
+
+    const make = document.getElementById('make').value;
+    const year = document.getElementById('year').value;
+    // Read the radio buttons
+    const level = document.querySelector('input[name="level"]:checked').value;
+
+    // Check that all the fields have something
+    if( make === '' || year === '' || level === '') {
+        console.log('Error');
+
+    } else {
+        console.log('Alright!!');
+    }
+   
+
+
+       });
+}
 
 
 
